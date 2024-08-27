961631ad2cf4ea4ae6b97e7efb3543b342199dec
yhb <1466450232@qq.com>
2024-08-27 19:59:21 +0800

    配置pom.xml以及创建相关文件夹

---
+++ src/main/java/com/firefly/sharecloud/ShareCloudApplication.java
@@ -0,0 +1,14 @@
+package com.firefly.sharecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class ShareCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareCloudApplication.class, args);
+    }
+}
